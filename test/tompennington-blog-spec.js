d7f9215763ba2e59fe9524594d133c2e698d6f3f
Tom Pennington <tompennington.92@gmail.com>
2016-07-11 09:01:15 +0100

    Fix tests before upgrading to webdriverio 4.

--- test/tompennington-blog-spec.js
+++ test/tompennington-blog-spec.js
@@ -1,34 +1,38 @@
-var assert                 = require("assert"),
-    testConfig             = require("./config/test-config.js"),
-    blogPageObject         = require("./page-objects/tompennington-blog.json"),
-    baseUrl                = testConfig.getBaseUrl();
+var assert = require("assert"),
+    testConfig = require("./config/test-config.js"),
+    blogPageObject = require("./page-objects/tompennington-blog.json"),
+    baseUrl = testConfig.getBaseUrl();
 
 describe("tompennington blog", function() {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 9999999;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 9999999;
 
-    beforeEach(function() {
-        client = testConfig.getWebdriver();
-        client.init().url(baseUrl);
-    });
+  beforeEach(function() {
+    client = testConfig.getWebdriver();
+    client.init().url(baseUrl);
+  });
 
-    it("should have a title tag set", function(done) {
-        client
-            .getTitle(function(err, title) {
-                expect(title).toEqual("Tom Pennington");
-            })
-            .call(done);
-    });
+  it("should have a title tag set", function(done) {
+    client
+      .getTitle(function(err, title) {
+        expect(title).toEqual("Tom Pennington");
+      })
+      .call(done);
+  });
 
-    it("author link should link to the about page", function(done) {
-        client
-            .click(blogPageObject.aboutLink)
-            .url(function(err, res) {
-                expect(res.value).toEqual("http://tompennington.co.uk/about-me/")
-            })
-            .call(done);
-    });
+  it("should show menu after clicking menu button", function(done) {
+    client
+      .isVisible(".nav").then(function(isVisible) {
+        expect(isVisible).toBe(false);
+      })
+      .click(".menu-button").then(function() {
+        this.isVisible(".nav").then(function(isVisible) {
+          expect(isVisible).toBe(true);
+        });
+      })
+      .call(done);
+  });
 
-    afterEach(function(done) {
-        client.end(done);
-    });
+  afterEach(function(done) {
+    client.end(done);
+  });
 });
